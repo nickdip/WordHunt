c69b183a83f6a80e1531efdddba1c87379d6130e
Nick Diplos <nickd321@gmail.com>
2023-09-03 20:50:34 +0100

    Adding board.js

---
+++ board.js
@@ -0,0 +1,72 @@
+class Board{
+    constructor(word){
+        this.root = {}
+        this.isWord = false
+        this.addWord(word)
+    }
+    
+    addWord(word){
+        let current = this.root
+        
+        for(const letter of word){
+            if(!current[letter]){
+                current[letter] = {}
+            }
+            current = current[letter]   
+        }
+        current.isWord = true
+    }
+    
+}
+
+
+const findWord = function(board, word) {
+    const board = new Board(word);
+
+    let result;
+    
+    const visited = [];
+    
+    const dfs = (i, j, node, check) => {
+
+        let ijCurrent = `${i}${j}`;
+
+        if(result || i < 0 || i >= board.length || j < 0 || j >= board[0].length || !node[board[i][j]] || visited.includes(ijCurrent)){
+            return
+        }
+        
+        visited.push(ijCurrent)
+        check += board[i][j]
+        
+        node = node[board[i][j]]
+        
+        if(node.isWord){
+           result = check
+           return   
+        }
+        
+        dfs(i, j+1, node, check)
+        dfs(i, j-1, node, check)
+        dfs(i-1, j, node, check)
+        dfs(i+1, j, node, check)
+        
+        ijCurrentIndex = visited.indexOf(ijCurrent);
+        visited.splice(ijCurrentIndex, 1);
+        
+    }
+    
+    for(let i = 0; i < board.length; i++){
+        for(let j = 0; j < board[0].length; j++){
+            if(board.root[board[i][j]]){
+                dfs(i, j, board.root, "")
+            }
+        }
+    }
+    
+    
+    return result
+
+};
+
+
+
