ef4ee0bb122ea70f780db66d98430700d4039ace
Nick Diplos <nickd321@gmail.com>
2023-09-07 18:49:50 +0100

    updating working board

--- board.js
+++ board.js
@@ -1,74 +1,61 @@
-class Board{
-    constructor(word){
-        this.root = {}
-        this.isWord = false
-        this.addWord(word)
-    }
-    
-    addWord(word){
-        let current = this.root
-        
-        for(const letter of word){
-            if(!current[letter]){
-                current[letter] = {}
-            }
-            current = current[letter]   
-        }
-        current.isWord = true
-    }
-    
-}
-
 
 function findWord(board, word) {
 
-    const searching = (i, j, node, check) => {
+    word = word.toLowerCase() 
 
-        let ijCurrent = `${i}${j}`;
+    let [ remainingWord, visited ] = [ word, [] ]
 
-        if(result || i < 0 || i >= board.length || j < 0 || j >= board[0].length || !node[board[i][j]] || visited.includes(ijCurrent)){
-            return
+    const  wordTrack = { letters: "", ijIndex: [] }
+
+    function searching(i, j) {
+        
+        if (wordTrack["letters"] == word) return true
+
+        let ijCurrent = `${i}${j}`
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited.includes(ijCurrent)){
+            return false
         }
         
         visited.push(ijCurrent)
-        check += board[i][j]
         
-        node = node[board[i][j]]
-        
-        if(node.isWord){
-           result = check
-           return   
+        letter = remainingWord[0] 
+
+        if (board[i][j] != letter) {
+            visited = []
+            return false 
         }
-        
-     searching(i, j+1, node, check)
-     searching(i, j-1, node, check)
-     searching(i-1, j, node, check)
-     searching(i+1, j, node, check)
-        
-        ijCurrentIndex = visited.indexOf(ijCurrent);
-        visited.splice(ijCurrentIndex, 1);
+
+        //change subword to Object with ij tracke
+        remainingWord = remainingWord.slice(1)
+        wordTrack["letters"] += letter
+        wordTrack["ijIndex"].push([i, j])
+
+        for (let const1 = -1; const1 <= 1; const1++) {
+            for (let const2 = -1; const2 <= 1; const2++) {
+                if (!(const1 == 0 && const2 == 0) && searching(i + const1, j + const2)) return true
+            }
+        }
+
+
+    visited = []
+        remainingWord = letter + remainingWord
+        wordTrack["letters"] = ""
         
     }
-    
-    const board = new Board(word);
-
-    let result;
-    
-    const visited = [];
-    
 
     for(let i = 0; i < board.length; i++){
         for(let j = 0; j < board[0].length; j++){
-            if(board.root[board[i][j]]){
-             searching(i, j, board.root, "")
-            }
+             if (searching(i, j)) return wordTrack
         }
     }
     
     
-    return result
+    return false 
 
 };
 
+board = [["C", "T", "L"], ["A", "S", "I"], ["M", "N", "T"]]
+console.log(findWord(board, "CAT")["letters"])
 
-
+module.exports = { findWord }
